8ec9217dbbb276b1f588769ab29baba7b2ca2c43
neelesh <neelesh.chavan@simulation-systems.co.uk>
2017-12-28 00:11:32 +0000

    Addind all dependencies

---
+++ reactivebasic/src/main/java/com/neelesh/reactivebasic/ReactivebasicApplication.java
@@ -0,0 +1,12 @@
+package com.neelesh.reactivebasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivebasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactivebasicApplication.class, args);
+	}
+}
---
+++ reactivebasic/src/test/java/com/neelesh/reactivebasic/ReactivebasicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.neelesh.reactivebasic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactivebasicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
