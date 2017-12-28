530bfe151f4a413267ea354f5374412603d0a475
neelesh <neelesh.chavan@simulation-systems.co.uk>
2017-12-28 00:26:56 +0000

    pushing gradle module

---
+++ movie-reactive-rest-mongodb/src/main/java/com/neelesh/moviereactiverestmongodb/MovieReactiveRestMongodbApplication.java
@@ -0,0 +1,12 @@
+package com.neelesh.moviereactiverestmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReactiveRestMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReactiveRestMongodbApplication.class, args);
+	}
+}
---
+++ movie-reactive-rest-mongodb/src/test/java/com/neelesh/moviereactiverestmongodb/MovieReactiveRestMongodbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.neelesh.moviereactiverestmongodb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MovieReactiveRestMongodbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
